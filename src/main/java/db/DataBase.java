ece0a1717a1ced1c285934d2321eeb09d0160d23
InSeong-So <goflvhxj2547@gmail.com>
2019-01-23 19:43:45 +0900

    h2 사용과 웹 어플리케이션 개발 구현 중

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -8,17 +8,20 @@ import com.google.common.collect.Maps;
 import model.User;
 
 public class DataBase {
-    private static Map<String, User> users = Maps.newHashMap();
+	private static Map<String, User> users = Maps.newHashMap();
+	static {
+		users.put("admin", new User("admin", "password", "테스터", "test@test.net"));
+	}
 
-    public static void addUser(User user) {
-        users.put(user.getUserId(), user);
-    }
+	public static void addUser(User user) {
+		users.put(user.getUserId(), user);
+	}
 
-    public static User findUserById(String userId) {
-        return users.get(userId);
-    }
+	public static User findUserById(String userId) {
+		return users.get(userId);
+	}
 
-    public static Collection<User> findAll() {
-        return users.values();
-    }
+	public static Collection<User> findAll() {
+		return users.values();
+	}
 }
