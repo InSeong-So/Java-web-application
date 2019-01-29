aeb10565c259cb91a0ec33477a424f3ac9ca4df0
InSeong-So <goflvhxj2547@gmail.com>
2019-01-29 19:19:34 +0900

    request, response 추가

---
+++ src/main/java/http/RequestLine.java
@@ -0,0 +1,31 @@
+package http;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestLine {
+	private static final Logger log = LoggerFactory.getLogger(RequestLine.class);
+
+	private String method;
+
+	private String path;
+
+	private Map<String, String> params;
+
+	RequestLine(String requestLine) {
+	}
+
+	String getMethod() {
+		return method;
+	}
+
+	String getPath() {
+		return path;
+	}
+
+	Map<String, String> getParams() {
+		return params;
+	}
+}
